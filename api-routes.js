3fa6cbadc482f3e7a1508838f37585b024dc1f60
yl-ang <yongliangang@gmail.com>
2022-09-15 01:01:21 +0800

    Update error handling for delete and update contacts

--- api-routes.js
+++ api-routes.js
@@ -1,27 +1,33 @@
-let router = require('express').Router();
+let router = require("express").Router();
 
 // Set default API response
-router.get('/', function (req, res) {
-    res.json({
-        status: 'API is up and working',
-        message: ''
-    });
+router.get("/", function (req, res) {
+  res.json({
+    status: "API is up and working",
+    message: "",
+  });
 });
 
 // Import contact controller
-var contactController = require('./contactController');
+var contactController = require("./contactController");
 
 // contact routes
-router.route('/contacts')
-    .get(contactController.index)
-    .post(contactController.new);
+router
+  .route("/contacts")
+  .get(contactController.index)
+  .post(contactController.new)
+  .all((req, res) => res.status(500).json({ error: "Internal Server Error" }));
 
+router
+  .route("/contacts/:contact_id")
+  .get(contactController.view)
+  .put(contactController.update)
+  .delete(contactController.delete)
+  .all((req, res) => res.status(500).json({ error: "Internal Server Error" }));
 
-router.route('/contacts/:contact_id')
-    .get(contactController.view)
-    .patch(contactController.update)
-    .put(contactController.update)
-    .delete(contactController.delete);
-
+router.use((err, req, res, next) => {
+  res.status(500).json({ error: "Internal Server Error" });
+  next(err);
+});
 // Export API routes
 module.exports = router;
--- contactController.js
+++ contactController.js
@@ -1,90 +1,98 @@
-const { json } = require('body-parser');
+const { json } = require("body-parser");
 
 // Import contact model
-Contact = require('./contactModel');
+Contact = require("./contactModel");
 
 // Handle index actions (retrieve)
 exports.index = function (req, res) {
-    Contact.get(function (err, contacts)  {
-        if (err) {
-            return res.status(500).json({message: 'Failed to retrieve contacts'})
-        }
-        res.json({
-            status: "success",
-            message: "contacts retrieved successfully",
-            data: contacts
-        });
+  Contact.get(function (err, contacts) {
+    if (err) {
+      return res.status(500).json({ message: "Failed to retrieve contacts" });
+    }
+    res.json({
+      status: "success",
+      message: "contacts retrieved successfully",
+      data: contacts,
     });
+  });
 };
 
 // Handles create contact actions
 exports.new = function (req, res) {
-    var contact = new Contact();
-    contact.name = req.body.name;
-    contact.gender = req.body.gender;
-    contact.email = req.body.email;
-    contact.phone = req.body.phone;
+  var contact = new Contact();
+  contact.name = req.body.name;
+  contact.gender = req.body.gender;
+  contact.email = req.body.email;
+  contact.phone = req.body.phone;
 
-    contact.save(function (err) {
-        if (err) {
-            return res.status(500).json({message: 'Failed to save new contact'})
-        }
+  contact.save(function (err) {
+    if (err) {
+      return res.status(500).json({ message: "Failed to save new contact" });
+    }
 
-        res.json({
-            message: "New contact created!",
-            data: contact
-        });
+    res.json({
+      message: "New contact created!",
+      data: contact,
     });
+  });
 };
 
 // Handles view contact info
 exports.view = function (req, res) {
-    Contact.findById(req.params.contact_id, function (err, contact) {
-        if (err) {
-            return res.status(500).json({message: 'Failed to view contact'})
-        }
-        res.json({
-            message: 'contact details loading ...',
-            data: contact
-        })
+  Contact.findById(req.params.contact_id, function (err, contact) {
+    if (err) {
+      return res.status(500).json({ message: "Failed to view contact" });
+    }
+
+    if (!contact) {
+      return res.status(500).json({ message: "Contact doesn't exist" });
+    }
+    res.json({
+      message: "contact details loading ...",
+      data: contact,
     });
+  });
 };
 
 // Handle update contact info
 exports.update = function (req, res) {
+  Contact.findById(req.params.contact_id, function (err, contact) {
+    if (err) {
+      return res.status(500).json({ message: "Failed to update contact" });
+    }
 
-    Contact.findById(req.params.contact_id, function (err, contact) {
-        if (err) {
-            return res.status(500).json({message: 'Failed to update contact'})
-        }
-        
-        contact.name = req.body.name ? req.body.name : contact.name;
-        contact.gender = req.body.gender ? req.body.gender : contact.gender;
-        contact.email = req.body.email ? req.body.email :  contact.email;
-        contact.phone = req.body.phone ? req.body.phone :  contact.phone;
+    if (!contact) {
+      return res.status(500).json({ message: "Contact doesn't exist" });
+    }
+    contact.name = req.body.name ? req.body.name : contact.name;
+    contact.gender = req.body.gender ? req.body.gender : contact.gender;
+    contact.email = req.body.email ? req.body.email : contact.email;
+    contact.phone = req.body.phone ? req.body.phone : contact.phone;
 
-        contact.save( function (err) {
-            if (err) {
-                return res.status(500).json({message: 'Failed to save updated contact to database'})
-            }
+    contact.save(function (err) {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "Failed to save updated contact to database" });
+      }
 
-            res.json({
-                message: "Contact Info updated",
-                data: contact
-            });
-        });
+      res.json({
+        message: "Contact Info updated",
+        data: contact,
+      });
     });
+  });
 };
 
 // Handle delete contact
 exports.delete = function (req, res) {
-    Contact.remove({ _id: req.params.contact_id}, function (err, contact) {
-        if (err) {
-            return res.status(500).json({message: 'Failed to delete contacts'})
-        }
-        res.json({
-            status: "success",
-            message: 'Contact deleted'
-        });
+  Contact.remove({ _id: req.params.contact_id }, function (err, contact) {
+    if (err) {
+      return res.status(500).json({ message: "Failed to delete contacts" });
+    }
+    res.json({
+      status: "success",
+      message: "Contact deleted",
     });
+  });
 };
